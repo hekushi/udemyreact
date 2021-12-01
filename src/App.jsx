29f846134febf4c1cfed5ae8cb5d6d4801f7edf8
hekushi <78855499+hekushi@users.noreply.github.com>
2021-12-02 08:14:35 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const App = () => {
+  return (
+    <>
+      <h1>こんにちわ</h1>
+      <p>お元気ですか？</p>
+      <button>ボタン</button>
+    </>
+  );
+};
+
+export default App;
