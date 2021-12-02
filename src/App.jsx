bb28b54759a23c461179916659c57c4a9c404028
hekushi <78855499+hekushi@users.noreply.github.com>
2021-12-03 06:17:55 +0900

    addZ1202

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+  const contentLedyStyle = {
+    color: "pink",
+    fontSize: "18px"
+  };
   return (
     <>
-      <h1>こんにちわ</h1>
-      <p>お元気ですか？</p>
-      <button>ボタン</button>
+      <h1 style={{ color: "red" }}>こんにちわ</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <p style={contentLedyStyle}>元気です</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
